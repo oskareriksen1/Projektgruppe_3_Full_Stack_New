332e38b2c7129d9e26a55cd134eb3e3a103691d0
Oskar Eriksen <oska1001@stud.kea.dk>
2024-04-16 12:44:43 +0200

    Initial commit

---
+++ src/main/java/dk/kea/projektgruppe_3_full_stack_new/Projektgruppe3FullStackNewApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.projektgruppe_3_full_stack_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projektgruppe3FullStackNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projektgruppe3FullStackNewApplication.class, args);
+    }
+
+}
